1c56c1fc80c675c2ac4d1d72e557fc711faab290
Roman <roman_ivanov3@epam.com>
2020-11-24 21:37:42 +0300

    add test jars

---
+++ README.md
@@ -0,0 +1,1 @@
+Ready-made assemblies for taking "risks" in the <a href="https://github.com/Drill4J">Drill</a></p> 
---
+++ start-0.6.0.bat
@@ -0,0 +1,6 @@
+SET PET_BUILD=0.6.0
+SET DISTR=agent
+SET ADMIN_ADDRESS=localhost:8090
+SET AGENT_ID=Petclinic
+SET START_PORT=8092
+java -agentpath:%DISTR%/drill_agent.dll=drillInstallationDir=%DISTR%/,adminAddress=%ADMIN_ADDRESS%,agentId=%AGENT_ID%,buildVersion=%PET_BUILD%,logLevel=DEBUG -jar spring-petclinic-%PET_BUILD%.BUILD-SNAPSHOT.jar --server.port=%START_PORT%
